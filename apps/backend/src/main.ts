080634a71e7a078c47c03c18faf668fb09493353
kelvinsgonzalez <125146867+kelvinsgonzalez@users.noreply.github.com>
2025-11-16 22:31:14 -0600

    subida a render
    render

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['https://internet-perla-sgi-1.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
